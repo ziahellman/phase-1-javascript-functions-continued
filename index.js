ce0ab4a413193ea0b985bd209cc9622df6cd85b9
Zia Hellman <ziahellman@gmail.com>
2022-05-25 21:12:47 +0300

    commit tree 75d72e8e2f7c0f16e527103894a381e55a080ae8

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+  return function (adjective = "special") {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`;
+  };
+}
+
+wrapAdjective("Hello")("hi");
